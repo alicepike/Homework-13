43c3e89239eda8677be975b5d8852557c5a4ffc9
Alice Pike <alicepike123@hotmail.com>
2013-03-17 16:25:59 +0000

    Updated js homework 13

--- script.js
+++ script.js
@@ -2,9 +2,25 @@ var $ = jQuery;
 
 // disable default action of 'submit' event
 
+$(".button").click(function(event){
+	event.preventDefault();
+});
+
 // validate that the use has:
 	// a username
+	function validateUsername(inputElement) {
+		var errors = [];
+		var value = inputElement.val();
+		if (value === "") {
+			errors.push("Username required");
+		}
+	}
 
 	// a password that matches the confirm password field
+	function validatePassword(inputElement) {
+		var errors = [];
+		var value = inputElement.val();
+		var value2 = 
+	}
 
 	// inputs that are all longer than 6 characters in length
