6025df988f623cfbd131813c72707961027e0ea3
Alice Pike <alicepike123@hotmail.com>
2013-03-17 14:31:21 +0000

    styled form

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 var $ = jQuery;
+
+// disable default action of 'submit' event
+
+// validate that the use has:
+	// a username
+
+	// a password that matches the confirm password field
+
+	// inputs that are all longer than 6 characters in length
